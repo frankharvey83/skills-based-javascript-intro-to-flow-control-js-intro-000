693164801983ac31c47cccf10893938eeed905ff
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-05 23:51:07 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,7 @@ if (age > 12 && age < 20) {
 function teenager(age)  {
 if (age > 12 && age < 20) {
   return ("You are a teenager!");
-} else if (age < 12 && age > 19)  {
+} else {
   return ("You are not a teenager");
   }
 }
