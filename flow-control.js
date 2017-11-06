a2ad72157715ed1c32204c3b3c85f808e95316e9
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-06 01:03:39 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -27,11 +27,11 @@ function ternaryTeenager(age) {
   return (age > 12 && age <20) ? "You are a teenager" : "You are not a teenager";
 }
 
-function switchAge(age) {
-  switch (age) {
-case  "teenager": age > 12;
-    return ("You are a teenager");
-case  "teenager1": age <20;
-    return ("You are a teenager");
-}
+function switchage(age) {
+  switch(age) {
+    case "teenager": age > 12 || age < 20;
+      return "You are a teenager";
+    default:
+      return "You have an age";
+  }
 }
